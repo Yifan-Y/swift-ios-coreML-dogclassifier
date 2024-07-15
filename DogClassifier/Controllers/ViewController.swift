8f8c3b7e4016565de3cf0fceda1c6dea386c325a
Yifan Yang <36191021+Yifan-Y@users.noreply.github.com>
2024-07-15 17:26:04 -0400

    Initial commit

---
+++ DogClassifier/Controllers/ViewController.swift
@@ -0,0 +1,174 @@
+//
+//  ViewController.swift
+//  DogClassifier
+//
+//  Created by Yifan Yang on 2024-07-15.
+//
+
+import UIKit
+import PhotosUI
+
+class ViewController: UIViewController {
+    var firstRun = true
+
+    let imagePredictor = Predictor()
+
+    let predictionsToShow = 3
+
+    @IBOutlet weak var startupPrompts: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var predictionLabel: UILabel!
+}
+
+extension ViewController {
+   
+    @IBAction func singleTap() {
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            present(photoPicker, animated: false)
+            return
+        }
+
+        present(cameraPicker, animated: false)
+    }
+
+    @IBAction func doubleTap() {
+        present(photoPicker, animated: false)
+    }
+}
+
+extension ViewController {
+
+    func updateImage(_ image: UIImage) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
+    }
+
+
+    func updatePredictionLabel(_ message: String) {
+        DispatchQueue.main.async {
+            self.predictionLabel.text = message
+        }
+
+        if firstRun {
+            DispatchQueue.main.async {
+                self.firstRun = false
+                self.predictionLabel.superview?.isHidden = false
+                self.startupPrompts.isHidden = true
+            }
+        }
+    }
+
+    func userSelectedPhoto(_ photo: UIImage) {
+        updateImage(photo)
+        updatePredictionLabel("Making predictions for the photo...")
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.classifyImage(photo)
+        }
+    }
+
+}
+
+extension ViewController {
+
+    private func classifyImage(_ image: UIImage) {
+        do {
+            try self.imagePredictor.makePredictions(for: image,
+                                                    completionHandler: imagePredictionHandler)
+        } catch {
+            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+        }
+    }
+
+
+    private func imagePredictionHandler(_ predictions: [Predictor.Prediction]?) {
+        guard let predictions = predictions else {
+            updatePredictionLabel("No predictions. (Check console log.)")
+            return
+        }
+
+        let formattedPredictions = formatPredictions(predictions)
+
+        let predictionString = formattedPredictions.joined(separator: "\n")
+        updatePredictionLabel(predictionString)
+    }
+
+
+    private func formatPredictions(_ predictions: [Predictor.Prediction]) -> [String] {
+        let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+            var name = prediction.classification
+
+            if let firstComma = name.firstIndex(of: ",") {
+                name = String(name.prefix(upTo: firstComma))
+            }
+
+            return "\(name) - \(prediction.confidencePercentage)%"
+        }
+
+        return topPredictions
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var cameraPicker: UIImagePickerController {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        return cameraPicker
+    }
+
+
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        picker.dismiss(animated: false)
+
+        guard let originalImage = info[UIImagePickerController.InfoKey.originalImage] else {
+            fatalError("Picker didn't have an original image.")
+        }
+
+        guard let photo = originalImage as? UIImage else {
+            fatalError("The (Camera) Image Picker's image isn't a/n \(UIImage.self) instance.")
+        }
+
+        userSelectedPhoto(photo)
+    }
+}
+extension ViewController: PHPickerViewControllerDelegate {
+    var photoPicker: PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        config.filter = PHPickerFilter.images
+
+        let photoPicker = PHPickerViewController(configuration: config)
+        photoPicker.delegate = self
+
+        return photoPicker
+    }
+
+
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: false)
+
+        guard let result = results.first else {
+            return
+        }
+
+        result.itemProvider.loadObject(ofClass: UIImage.self) { object, error in
+            if let error = error {
+                print("Photo picker error: \(error)")
+                return
+            }
+
+            guard let photo = object as? UIImage else {
+                fatalError("The Photo Picker's image isn't a/n \(UIImage.self) instance.")
+            }
+
+            self.userSelectedPhoto(photo)
+        }
+    }
+}
+
+
+
---
+++ DogClassifierTests/DogClassifierTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogClassifierTests.swift
+//  DogClassifierTests
+//
+//  Created by Yifan Yang on 2024-07-15.
+//
+
+import Testing
+@testable import DogClassifier
+
+struct DogClassifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
